282b3669e071385bff64d335ea516226dc707286
Mauricio Cruz <mawcp7@gmail.com>
2023-08-09 11:31:47 -0600

    add cody sticker to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-angular-project';
+  imgCody = 'assets/codys/1.png';
+
+  ngOnInit(){
+    this.getCodyImg();
+  }
+
+  getCodyImg(){
+    let rand = Math.random() * 16 + 1;
+    rand = Math.floor(rand);
+    this.imgCody = `assets/codys/${rand}.png`;
+  }
 }
