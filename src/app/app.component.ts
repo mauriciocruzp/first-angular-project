3cdd0155b2622e1978bd6d0ef7432f8301cc7fe7
Mauricio Cruz <mawcp7@gmail.com>
2023-08-12 14:16:46 -0600

    remove cody sticker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'first-angular-project';
-  imgCody = 'assets/codys/1.png';
-
-  ngOnInit(){
-    this.getCodyImg();
-  }
-
-  getCodyImg(){
-    let rand = Math.random() * 16 + 1;
-    rand = Math.floor(rand);
-    this.imgCody = `assets/codys/${rand}.png`;
-  }
 }
