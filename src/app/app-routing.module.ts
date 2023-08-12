3d4cc56b38f5bfee4eb708429f93897db06de930
Mauricio Cruz <mawcp7@gmail.com>
2023-08-12 14:16:13 -0600

    add router module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+import { DetailsComponent } from './pages/details/details.component';
+
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'card/:id', component: DetailsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
